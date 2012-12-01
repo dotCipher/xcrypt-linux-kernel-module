936b42f7ce36190e1248a48c576f2a109db8eef6
root <root@vl121.cs.stonybrook.edu>
2012-12-01 00:22:23 -0500

    Added some basic command line interpretations.

--- hw3/xcipher.c
+++ hw3/xcipher.c
@@ -4,23 +4,132 @@
 #include <errno.h>
 
 #include "sys_xcrypt.h"
+#include "xcipher.h"
 
-#define __NR_xcrypt	349	/* our private syscall number */
-
-int main(int argc, char *argv[])
-{
-	int xcrypt_rv;
-    struct xcrypt_params *params;
-	void *param_ptr;
+void init_xcrypt_params(struct xcrypt_params *ptr){
+	int i;
 	
-	params->infile = malloc(sizeof(char)*256);
-	//params->infile = memset(0);
-	params->infile = argv[1];
-	param_ptr = (void *) params;
+	ptr->outfile = malloc(sizeof((char)*OUTFILE_MAX));
+	memset(ptr->outfile, 0, sizeof(strlen(ptr->outfile)));
+	
+	ptr->infile = malloc(sizeof((char)*INFILE_MAX));
+	memset(ptr->infile, 0, sizeof(strlen(ptr->infile)));
+	
+	ptr->keybuf = malloc(sizeof((char)*PASS_MAX));
+	memset(ptr->keybuf, 0, sizeof(strlen(ptr->keybuf)));
+	
+	ptr->keylen = 0;
+	
+	for(i = 0; i < 8; i++){
+		ptr->flags &= ~(1 << i)
+	} 
+}
+
+#ifdef EXTRA_CREDIT
+void init_valid_algorithms(char *algs[]){
+	int i;
+	/* Intialize and clear the whole array */
+	for(i = 0; i < VALID_ALG_NUM; i++){
+		if((algs[i] = (char*)malloc(sizeof((char)*16))) != NULL){
+			memset(algs[i], 0, sizeof((char)*16));
+		} else {
+			perror("Error allocating memory for valid_algorithms: ");
+			return;
+		}	
+	}
+	/* Set all ciphers here */
+	algs[0] = "aes";
+	algs[1] = "anubis";
+	algs[2] = "arc4";
+	algs[3] = "blowfish";
+	algs[4] = "cast5";
+	algs[5] = "cast6";
+	algs[6] = "des";
+	algs[7] = "des3_ede";
+	algs[8] = "khazad";
+	algs[9] = "serpent";
+	algs[10] = "twofish";
+	/* Return when done */
+	return;
+}
+#endif
+
+int main(int argc, char *argv[]){
+	/* Basic counter */
+	int i;
+	/* System Call Return Value */
+	int xcrypt_rv;
+	/* All Params Stored in Structure */
+    struct xcrypt_params *params;
+    /* Void Pointer to be passed to System Call */
+	void *params_ptr;
+	/* Booleans for flags */
+	int opt, enc, dec, cip, pas, hlp;
+	/* Error checking flags */
+	int pasArgNum = -1;
+	/* Valid ciphers */
+	#ifdef EXTRA_CREDIT
+		int alg_num = -1;
+		char *valid_algs[VALID_ALG_NUM];
+		init_valid_algorithms(valid_algs);
+	#endif
+	
+	/* Parse out and check all parameters */
+	opt = 0; enc = 0; dec = 0; cip = 0; pas = 0; hlp = 0;
+	while((opt = getopt(argc, argv, "dec:hp:")) != -1){
+		switch(opt){
+			case 'd':
+				dec = 1;
+				break;
+			case 'e':
+				enc = 1;
+				break;
+			case 'h':
+				hlp = 1;
+				break;
+			case 'p':
+				if(strlen(optarg) > PASS_MAX){
+					fprintf(stderr, "Error: Password cannot exceed %d characters\n", PASS_MAX);
+					exit(EXIT_PASS_ERR);
+				} else {
+					strcpy(params->keybuf, optarg);
+					pasArgNum = optind-1;
+					pas = 1;
+					break;
+				}
+			case 'c':
+				#ifdef EXTRA_CREDIT
+					for(i = 0; i < VALID_ALG_NUM; i++){
+						if(strcmp(optarg, valid_algs[i]) == 1){
+							alg_num = i;
+						}
+					}
+					if(alg_num == -1){
+						fprintf(stderr, "Error: Invalid algorithm argument specified for -c\n");
+						exit(EXIT_CIPHER_ERR);
+					}
+				#else
+					goto default;
+				#endif
+			default:
+				fprintf(stderr, "Error: Invalid execution\n");
+				fprintf(stderr,"Usage: %s [OPTIONS] [-p PASSWORD] <infile> <outfile>\n", argv[0]);
+				exit(EXIT_INPUT_ERR);
+		}
+	}
+	
+	/* Start main execution */
+	if(hlp == 1){
+		
+	}
+	
+	params_ptr = (void *) params;
 	
 	printf("Test return: %s\n", params->infile);
 	
-  	//xcrypt_rv = syscall(__NR_xcrypt, param_ptr);
+	
+	xcrypt_rv = 0;
+  	//xcrypt_rv = syscall(__NR_xcrypt, params_ptr);
 	//printf("system call returned with %d\n", xcrypt_rv);
 	
 	//free(params->infile);
---
+++ hw3/xcipher.h
@@ -0,0 +1,22 @@
+/* Private system call number */
+#define __NR_xcrypt 349
+
+/* Extra Credit Boolean */
+#define EXTRA_CREDIT 1
+
+/* Max values */
+#define PASS_MAX 64
+#define INFILE_MAX 256
+#define OUTFILE_MAX 256
+
+/* Numerical helpers */
+#define VALID_ALG_NUM 11
+
+/* Error codes */
+#define EXIT_CIPHER_ERR -5
+#define EXIT_OUTFILE_ERR -4
+#define EXIT_INFILE_ERR -3
+#define EXIT_PASS_ERR -2
+#define EXIT_INPUT_ERR -1
+#define EXIT_SUCCESS 0
+#define EXIT_HELP 1
