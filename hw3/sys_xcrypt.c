4c6e4d2f1fd2dca18493122dacd4d0f566f20d7f
root <root@vl121.cs.stonybrook.edu>
2012-12-09 13:57:35 -0500

    Fixed bug

--- hw3/sys_xcrypt.c
+++ hw3/sys_xcrypt.c
@@ -98,7 +98,10 @@ asmlinkage int sys_xcrypt(void *args){
 	char *k_keybuf;
 	int k_keylen;
 	unsigned char k_flags;
+	long page_size;
+	char *buffer;
 	int params_len = sizeof(struct xcrypt_params);
+	page_size = PAGESIZE;
 	printk(KERN_CRIT "--- Entering kerning module sys_xcrypt ---\n");
 
 	// Check if the void pointer is from userspace is readable
@@ -114,72 +117,51 @@ asmlinkage int sys_xcrypt(void *args){
 		return -EINVAL;
 	}
 	
-	// Alloc memory for the parts of the struct 
-	//   that the void pointer is pointing too
-	if(!(k_infile = kmalloc(INFILE_MAX, GFP_KERNEL))){
+	// Alloc memory and retrive data
+	// keybuf
+	if(!(k_keybuf = kmalloc(PASS_MAX, GFP_KERNEL))){
 		kfree(k_args);
 		return -ENOMEM;
 	}
-	if(!(k_outfile = kmalloc(OUTFILE_MAX, GFP_KERNEL))){
-		kfree(k_args); kfree(k_infile);
-		return -ENOMEM;
-	}
-	if(!(k_keybuf = kmalloc(PASS_MAX, GFP_KERNEL))){
-		kfree(k_args); kfree(k_infile);
-		kfree(k_outfile);
-		return -ENOMEM;
-	}
-	
-	// Get the values held by the struct
-	k_infile = getname(((struct xcrypt_params *)k_args)->infile);
-	k_outfile = getname(((struct xcrypt_params *)k_args)->outfile);
 	if(!access_ok(VERIFY_READ, ((struct xcrypt_params *)k_args)->keybuf,
 	((struct xcrypt_params *)k_args)->keylen)){
-		kfree(k_args); kfree(k_infile);
-		kfree(k_outfile); kfree(k_keybuf);
+		kfree(k_args); kfree(k_keybuf);
 		return -EFAULT;
 	}
 	if(copy_from_user(k_keybuf, ((struct xcrypt_params *)k_args)->keybuf,
-	((struct xcrypt_params*)k_args)->keylen)){
-		kfree(k_args); kfree(k_infile);
-		kfree(k_outfile); kfree(k_keybuf);
+	((struct xcrypt_params *)k_args)->keylen)){
+		kfree(k_args); kfree(k_keybuf);
 		return -EINVAL;
 	}
-	if(copy_from_user(k_flags, ((struct xcrypt_params *)k_args)->flags,
-	sizeof(unsigned char))){
-		kfree(k_args); kfree(k_infile);
-		kfree(k_outfile); kfree(k_keybuf);
-		return -EINVAL;
-	}
+	// keylen
 	/*
-	if(strncpy_from_user(k_infile, 
-	((struct xcrypt_params *)args)->infile, INFILE_MAX)){
-		return -EFAULT;
-	}
-	if(strncpy_from_user(k_outfile, 
-	((struct xcrypt_params *)args)->outfile, OUTFILE_MAX)){
-		return -EFAULT;
-	}
-	if(strncopy_from_user(k_keybuf,
-	((struct xcrypt_params *)args)->keybuf, PASS_MAX)){
-		return -EFAULT;
-	}
 	if(copy_from_user(k_keylen,
 	((struct xcrypt_params *)args)->keylen, sizeof(int))){
 		return -EFAULT;
 	}
+	*/
+	// Infile and outfile 
+	k_infile = getname(((struct xcrypt_params *)k_args)->infile);
+	k_outfile = getname(((struct xcrypt_params *)k_args)->outfile);
+
+	/*
 	if(copy_from_user(k_flags,
 	((struct xcrypt_params *)args)->flags, sizeof(unsigned char))){
 		return -EFAULT;
 	}
 	*/
+	
 	// Dissect the void pointer
+
 	printk(KERN_CRIT "k_infile = %s\n", k_infile);
 	printk(KERN_CRIT "k_outfile = %s\n", k_outfile);
 	printk(KERN_CRIT "k_keybuf = %s\n", k_keybuf);
+	/*
 	printk(KERN_CRIT "k_keylen = %d\n", k_keylen);
 	printk(KERN_CRIT "k_flags = %d\n", k_flags);
-		
+	*/	
+	kfree(kargs); kfree(k_keybuf);
+	kfree(k_infile); kfree(k_outfile);
 	return 0;
 }
 
